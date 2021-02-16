1380506cf782d9207a64fd7fd75894d5593ecb05
Kevin K <kbknapp@gmail.com>
2021-02-15 23:24:24 -0500

    stubs the run

--- src/cmds/run.rs
+++ src/cmds/run.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-
+use tokio::signal::unix::{signal, SignalKind};
 use yaml_rust::YamlLoader;
 
 use crate::{
@@ -8,16 +8,17 @@ use crate::{
     device::{Device, Devices},
     cli::RunArgs,
     rule::{Rule, Rules},
-    listen::listen,
+    //listener::listen,
 };
 
 pub fn run(a: RunArgs) {
-    let buf = fs::read_to_string(a.rules).unwrap();
-    let _rules = Rules::from(&YamlLoader::load_from_str(&*buf).unwrap()[0]);
+    // let hup = signal(SignalKind::hangup()).unwrap();
+    // let buf = fs::read_to_string(a.rules).unwrap();
+    // let _rules = Rules::from(&YamlLoader::load_from_str(&*buf).unwrap()[0]);
 
-    tokio::runtime::Builder::new_current_thread()
-        .enable_io()
-        .build()
-        .unwrap()
-        .block_on(listen())
+    // tokio::runtime::Builder::new_current_thread()
+    //     .enable_io()
+    //     .build()
+    //     .unwrap()
+    //     .block_on(listen())
 }
