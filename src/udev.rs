8ac75c2c87f93d798ab6e3b8397dab5973e8d925
Kevin K <kbknapp@gmail.com>
2021-02-16 22:28:42 -0500

    factors out UdevEvent

---
+++ src/udev.rs
@@ -0,0 +1,31 @@
+use serde::{Serialize};
+
+use crate::usb::{UsbEvent, UsbDevice, UsbPort};
+
+#[derive(Clone, PartialEq, Debug, Serialize)]
+pub struct UdevEvent {
+    pub event_kind: UsbEvent,
+    pub device: UsbDevice,
+    pub port: UsbPort,
+}
+
+impl UdevEvent {
+    pub fn new(event_kind: UsbEvent) -> Self {
+        Self {
+            event_kind,
+            device: UsbDevice::default(),
+            port: UsbPort::default(),
+        }
+    }
+}
+
+impl From<tokio_udev::Event> for UdevEvent {
+    fn from(e: tokio_udev::Event) -> Self {
+        let d = e.device();
+        Self {
+            event_kind: e.event_type().into(),
+            device: UsbDevice::from(&d),
+            port: UsbPort::from(&d),
+        }
+    }
+}
